8f6dfb0207339feeef60568b6fcad4a42773a788
danielmjohnston <m193072@usna.edu>
2017-02-09 14:22:58 -0500

    Add files via upload
    Added skeleton of Rotor class

---
+++ Rotor.java
@@ -0,0 +1,17 @@
+public class Rotor
+{
+  char[] letters;
+  int rotation;
+
+  public Rotor makeRotor(String s){
+  }
+
+  public char charAt(int n){
+  }
+
+  public int indexOf(char c){
+  }
+
+  public void rotate(){
+  }
+}
